fd02a874373ab97bebc4b85ad4fbac26bda1446d
徐仁海 <645213400@qq.com>
2019-01-07 14:39:11 +0800

    提交

--- src/main/java/com/itheima/controller/BaseController.java
+++ src/main/java/com/itheima/controller/BaseController.java
@@ -3,5 +3,6 @@ package com.itheima.controller;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("LLLLLLLLLLL");
+        System.out.println("LLLLLLLLLLLL");
     }
 }
